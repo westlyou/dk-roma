f19da366eadb05baf45e564b2bf1cb9ee9db08bd
Oaken <oladelesamson37@yahoo.com>
2020-12-30 11:54:08 +0100

    grid refactored

--- dkaroma/static/js/script.js
+++ dkaroma/static/js/script.js
@@ -1,56 +1,52 @@
+var $grid;
+
+var gutterSize = 30;
+if (window.matchMedia('(max-width: 1024px)').matches && window.matchMedia('(min-width: 768px)').matches) {
+    gutterSize = 10;
+}
+
+if (window.matchMedia('(min-width: 1024px)').matches) {
+    gutterSize = 30;
+}
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+    gutterSize = 10;
+    // itemFitWidth = true;
+}
+
+
+function getColumnWidthSelector() {
+
+    if ($('.search-result-items').children().length == 1) {
+        return '.search-result-items.grid-wrap .item:nth-of-type(1)';
+    }
+
+    return '.search-result-items.grid-wrap .item:nth-of-type(2)';
+}
+
+$grid = $('.search-result-items').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.search-result-items.grid-wrap .item',
+    // use element for option
+    columnWidth: getColumnWidthSelector(),
+    percentPosition: true,
+    horizontalOrder: true,
+    gutter: gutterSize,
+    transitionDuration: '0.9s',
+    stagger: 30
+});
+
+$('.search-result-items').imagesLoaded(function() {
+    $grid.masonry();
+});
+
+
+$(window).on('load', function() {
+    $grid.masonry();
+});
+
 $(document).ready(function() {
-    var $grid;
 
-    var gutterSize = 30;
-    if (window.matchMedia('(max-width: 1024px)').matches && window.matchMedia('(min-width: 768px)').matches) {
-        gutterSize = 10;
-    }
-
-    if (window.matchMedia('(min-width: 1024px)').matches) {
-        gutterSize = 30;
-    }
-
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        gutterSize = 10;
-        // itemFitWidth = true;
-    }
-
-
-    function getColumnWidthSelector() {
-
-        if ($('.search-result-items').children().length == 1) {
-            return '.search-result-items.grid-wrap .item:nth-of-type(1)';
-        }
-
-        return '.search-result-items.grid-wrap .item:nth-of-type(2)';
-    }
-
-    // $grid = $('.search-result-items').masonry({
-    //     // set itemSelector so .grid-sizer is not used in layout
-    //     itemSelector: '.search-result-items.grid-wrap .item',
-    //     // use element for option
-    //     columnWidth: getColumnWidthSelector(),
-    //     percentPosition: true,
-    //     horizontalOrder: true,
-    //     gutter: gutterSize,
-    //     transitionDuration: '0.9s',
-    //     stagger: 30
-    // });
-
-
-    $('.search-result-items').imagesLoaded(function() {
-        $grid = $('.search-result-items').masonry({
-            // set itemSelector so .grid-sizer is not used in layout
-            itemSelector: '.search-result-items.grid-wrap .item',
-            // use element for option
-            columnWidth: getColumnWidthSelector(),
-            percentPosition: true,
-            horizontalOrder: true,
-            gutter: gutterSize,
-            transitionDuration: '0.9s',
-            stagger: 30
-        });
-    });
 
     $(window).resize(function() {
 
