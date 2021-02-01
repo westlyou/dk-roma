0ce4ceb58de5e12fde31a6c7cbe0d7617cbb417f
Oaken <oladelesamson37@yahoo.com>
2021-02-01 10:46:05 +0100

    value changes

--- dkaroma/static/js/main.js
+++ dkaroma/static/js/main.js
@@ -80,7 +80,7 @@ const API_VALUES = {
 
 function addProductToCart(pid) {
 
-    var cartUrl = URL_VALUES.base_url + "/shop/cart/update";
+    var cartUrl = API_VALUES.base_url + "/shop/cart/update";
 
     $.ajax({
         method: "POST",
