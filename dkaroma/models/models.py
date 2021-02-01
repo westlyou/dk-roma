2385d8d1b96711a9c1c6f7dba2b3c47fac7d2556
Evans Ehiorobo <ehioroboevans@gmail.com>
2021-02-01 11:45:26 +0100

    Added backend for home page.

--- dkaroma/models/models.py
+++ dkaroma/models/models.py
@@ -1,18 +1,50 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class dkaroma(models.Model):
-#     _name = 'dkaroma.dkaroma'
-#     _description = 'dkaroma.dkaroma'
+class ECommerce(models.Model):
+    _name = 'dkaroma.home'
+    _description = 'Home Page'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char("Name")
+    banner_text_small_1 = fields.Char()
+    banner_text_small_2 = fields.Char()
+    banner_image_1 = fields.Binary()
+    banner_image_2 = fields.Binary()
+    
+    header_text_big = fields.Char()
+    header_text_small = fields.Char()
+    
+    card_1_text_big = fields.Char()
+    card_1_text_small = fields.Char()
+    card_1_image = fields.Binary()
+
+    card_2_text_big = fields.Char()
+    card_2_text_small = fields.Char()
+    card_2_image = fields.Binary()
+
+    scroll_products_1_text_big = fields.Char()
+    scroll_products_1_text_small = fields.Char()
+    scroll_products_1 = fields.Many2many("product.template", "scroll_products_1_rel")
+
+    card_3_text_big = fields.Char()
+    card_3_text_small = fields.Char()
+    card_3_image = fields.Binary()
+
+    card_4_text_big = fields.Char()
+    card_4_text_small = fields.Char()
+    card_4_image = fields.Binary()
+
+    card_5_text_big = fields.Char()
+    card_5_text_small = fields.Char()
+    card_5_image = fields.Binary()
+
+    card_6_text_big = fields.Char()
+    card_6_text_small = fields.Char()
+    card_6_image = fields.Binary()
+
+    scroll_products_2_text_big = fields.Char()
+    scroll_products_2_text_small = fields.Char()
+    scroll_products_2 = fields.Many2many("product.template", "scroll_products_2_rel")
+
