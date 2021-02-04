e767eec6801cdd8f18bdbc8db680c089c7920cfc
Evans Ehiorobo <ehioroboevans@gmail.com>
2021-02-04 13:05:10 +0100

    Fixed customisation bugs.

--- dk_contacts_customization/models/models.py
+++ dk_contacts_customization/models/models.py
@@ -10,11 +10,9 @@ class Contacts(models.Model):
     mobile = fields.Char()
     email = fields.Char()
 
-    _sql_constraints = [
-        ('email_unique', 'unique(email)', 'Email address must be unique!'),
-        ('phone_unique', 'unique(phone)', 'Phone number must be unique!'),
-        ('mobile_unique', 'unique(mobile)', 'Mobile number must be unique!'),
-    ]
+    def email_unique(self): pass
+    def phone_unique(self): pass
+    def mobile_unique(self): pass
 
 
 
